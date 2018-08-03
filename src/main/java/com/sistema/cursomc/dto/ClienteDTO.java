bbae4d82560b0a4e3d51927684fbd749ba941bd4
kleyton032 <kleyton.joao@gmail.com>
2018-08-02 22:49:13 -0300

    Validação customizada p/update email

--- src/main/java/com/sistema/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/sistema/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.sistema.cursomc.model.Cliente;
+import com.sistema.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/sistema/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,63 @@
+package com.sistema.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.sistema.cursomc.dto.ClienteDTO;
+import com.sistema.cursomc.dto.ClienteNewDTO;
+import com.sistema.cursomc.model.Cliente;
+import com.sistema.cursomc.model.enums.TipoCliente;
+import com.sistema.cursomc.repositories.ClienteRepository;
+import com.sistema.cursomc.resources.exceptions.FieldMessage;
+import com.sistema.cursomc.services.validation.utils.CpfOuCnpj;
+
+//classe de validação customizada para inserção de um novo cliente para validar o cpf ou cnpj
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO clienteDto, ConstraintValidatorContext context) {
+		
+		//pegando o id para update na url 
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		//usando um lista de fieldmessage classe auxiliar para mostrar apenas o campo e msg de erro.
+		List<FieldMessage> list = new ArrayList<>();
+		
+		//condição p/testar se o e-mail já existe cadastrado ou não.
+		Cliente cliente = clienteRepository.findByEmail(clienteDto.getEmail());
+		
+		if(cliente !=null && !cliente.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
+		}
+		
+		//código auxiliar para que o framework consiga fazer a verificação e validação do codigo acima.
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
